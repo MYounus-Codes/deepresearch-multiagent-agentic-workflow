af43f65fc89febf2c8e4edad24f599066093ea0b
M. Yousuf <yousufhere.dev@gmail.com>
2026-06-18 13:26:06 +0500

    web_search tool added

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from deepresearch-multiagent-project!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools.py
@@ -0,0 +1,27 @@
+from langchain.tools import tool
+from tavily import TavilyClient
+from bs4 import BeautifulSoup
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool()
+def search_web(query: str) -> str:
+    """Search the web for the given query and return the results. RETURN URL, TITLE, AND SNIPPET"""
+    response = tavily.search(query=query, max_results=5)
+
+    out = []
+
+    for r in response["results"]:
+        out.append(
+            f"Title: {r['title']}\n"
+            f"URL: {r['url']}\n"
+            f"Snippet   : {r['content'][:400]}\n"
+        )
+    
+    return "\n---\n".join(out)
+
+print(search_web.invoke("Give me status of Porugal vs DR Congo match."))
